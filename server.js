04a674b878a527dde866d582fc9f3a0b7e2dcb4f
uncommon-type <uncommon.type@rambler.ru>
2024-02-12 11:02:41 +0300

    [#9] Set up CORS
    - Install cors
    
    - Define corsOptions object and specify origin (http://localhost:5173)
    to allow requests only from specified origin
    
    - Apply cors to Express app before other middleware, using
    app.use(cors(corsOptions))

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import logger from 'morgan';
+import cors from 'cors';
 
 import { router } from './routes.js';
 import { problemTypes } from './config/problem-types.js';
 import { configureProblemDetailsResponse } from './middleware/problem-details-response.js';
 import { notFound } from './middleware/error.js';
 
+const corsOptions = {
+  origin: 'http://localhost:5173'
+};
+
 const app = express();
 const port = 3000;
+
+app.use(cors(corsOptions));
+
 const problemDetailsResponseMiddleware =
   configureProblemDetailsResponse(problemTypes);
 
